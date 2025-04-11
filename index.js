723bc0709e5afa92e2324861e4882de7788f510e
Anna Andrade <anna.1504@outlook.com>
2025-04-11 20:52:04 -0300

    variaveis

---
+++ index.js
@@ -0,0 +1,81 @@
+/*console.log(`aoba`)
+
+
+//soma
+let primeiroNumero = 18
+let segundoNumero = 7
+
+let soma = primeiroNumero + segundoNumero
+
+console.log('Soma:',soma)
+
+//subtração
+let subtracao = primeiroNumero - segundoNumero
+
+console.log('Subtração:', subtracao)
+
+//Multiplicação
+let multiplicacao = primeiroNumero * segundoNumero
+
+console.log('Multiplicação:', multiplicacao)
+
+//Divisão
+let divisao = primeiroNumero / segundoNumero
+
+console.log('Divisão:', divisao)
+
+//Exponenciação ou Potência
+let potencia = primeiroNumero ** segundoNumero
+
+console.log('Potência:', potencia)
+
+//Módulo ou Resto da divisão
+let modulo = primeiroNumero % segundoNumero
+
+console.log('Módulo:', modulo)
+
+
+
+// Como calcular e atribuir ao mesmo tempo
+let a = 6
+let b = 28
+
+//soma guardando resultado em b
+
+b += a
+console.log('Soma:', b)
+
+//subtração guardando resultado em b
+b -= a
+console.log('Subtração:', b)
+
+//divisão guardando resultado em b
+ b/= a
+console.log('Divisão:', b)
+
+//Multiplicação guardando o resultado em b
+b*=a
+console.log("Multiplicação:", b)
+
+//Incremento
+a += 1
+console.log(a)
+
+a++
+console.log(a)
+
+//Decremento
+
+b -= 1
+console.log(b)
+
+b--
+console.log(b)*/
+
+let nome = "Anna"
+let sobrenome = "Andrade"
+let idade = 25
+let cidade = "Belford Roxo"
+let hobby = "cerâmica"
+
+console.log("Eu sou " + nome + " " + sobrenome + ", meu hobby é " + hobby + " e eu tenho" + " " + idade + " " + "anos.")
